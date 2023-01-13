cfa3965c740fb7906522cb1b34c54bc4dcfe5536
Alexfm101 <fraumalex@gmail.com>
2023-01-13 10:54:17 -0400

    pass some keymaps to keymaps lua

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,14 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 
+-- open the current file in the default program
+vim.keymap.set('n', '<Leader>x', ':xdg-open %<CR><CR>') 
+-- insert mode
+vim.keymap.set('i', 'aa', '<Esc>')
+--toggle tree view
+vim.keymap.set('n', '<Leader>n', ':NERDTreeToggle<Enter>')
+-- easy motion
+vim.keymap.set('n', '<Leader>s', '<Plug>(easymotion-s2)')
+--move code blocks in visual mode
+vim.keymap.set('x', 'K :move', "'<-2<CR>gv-gv")
+vim.keymap.set('x', 'J :move', "'>+1<CR>gv-gv")
