3ae79fa6f2fd3c350e17f40e75100a4b353b647e
Alexfm101 <fraumalex@gmail.com>
2023-01-16 23:26:03 -0400

    change telescope

--- nvim/lua/config/telescope.lua
+++ nvim/lua/config/telescope.lua
@@ -1,1 +1,7 @@
-
+require('telescope').setup{
+    defaults = {
+        file_ignore_patterns = {"node_modules", "vendor"},
+        border = false,
+        sorting_strategy = "ascending"
+    }
+}
